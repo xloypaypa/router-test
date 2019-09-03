1a4a3f156fdb31790a24021a49e3f8e1715dada5
xsu <xsu@thoughtworks.com>
2019-09-03 23:00:01 +0800

    动态路由

--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,13 @@ import VueRouter from 'vue-router'
 
 import Foo from "../components/Foo";
 import Bar from "../components/Bar";
+import Profile from "../components/Profile";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     routes: [
+        { path: '/profile/:username', name: 'profile', component: Profile },
         { path: '/foo', name: 'foo', component: Foo },
         { path: '/toFoo', redirect: {name: 'foo'} },
         { path: '/bar', name: 'bar', component: Bar }
