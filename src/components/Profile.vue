4e53b92238ee5820b242b7f8cfd66ac313bc39c9
xsu <xsu@thoughtworks.com>
2019-09-03 23:05:42 +0800

    ...profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <span>{{getUserNameFromUrl}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Profile',
+    computed: {
+        getUserNameFromUrl: function() {
+            return this.$route.params.username;
+        }
+    }
+}
+</script>
