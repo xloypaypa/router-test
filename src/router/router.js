29e5451703d0ccf2b3519ce000999079c321351d
xsu <xsu@thoughtworks.com>
2019-09-03 22:44:19 +0800

    redirect.

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Foo from "../components/Foo";
+import Bar from "../components/Bar";
+
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     routes: [
-        { path: '/foo', component: () => import("@/components/Foo") },
-        { path: '/bar', component: () => import("@/components/Bar") }
+        { path: '/foo', component: Foo },
+        { path: '/toFoo', redirect: '/foo' },
+        { path: '/bar', component: Bar }
       ]
 })
 
